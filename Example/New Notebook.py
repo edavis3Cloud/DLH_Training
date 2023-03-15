9c5bf6e5f008b92793d915ca1f7c9d46596cc1c2
edavis3Cloud <edavis3Cloud>
2023-03-15 20:43:52 +0000

    added sample select

--- Example/New Notebook.py
+++ Example/New Notebook.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC SELECDT * FROM sometablename
+# MAGIC SELECT * FROM delta.`/databricks-datasets/nyctaxi-with-zipcodes/subsampled`
+
+# COMMAND ----------
+
+files = dbutils.fs.ls("/databricks-datasets/nyctaxi-with-zipcodes/subsampled")
+display(files)
